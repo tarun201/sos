c009e5c249137a61473754f1bc168adebe96e1a3
imranmohsin18 <crazyimran18@gmail.com>
2016-11-19 12:57:13 +0530

    First commit

--- app/src/main/java/com/example/mims/sos/Home.java
+++ app/src/main/java/com/example/mims/sos/Home.java
@@ -1,6 +1,8 @@
 package com.example.mims.sos;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 /**
  * Created by MIMs on 11/19/2016.
@@ -12,5 +14,11 @@ public class Home extends MainActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+    }
+
+    public void mainactivity (View view)
+    {
+        Intent intent = new Intent(this,MainActivity.class);
+        startActivity(intent);
     }
 }
