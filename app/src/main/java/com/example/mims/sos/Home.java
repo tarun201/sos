85c64e6488c949901fa8352737ff28935ff74b15
Tarun Goenka <tarun.goenka12@gmail.com>
2016-11-20 12:13:35 +0530

    First Commit

--- app/src/main/java/com/example/mims/sos/Home.java
+++ app/src/main/java/com/example/mims/sos/Home.java
@@ -1,6 +1,7 @@
 package com.example.mims.sos;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 
@@ -20,5 +21,12 @@ public class Home extends MainActivity
     {
         Intent intent = new Intent(this,MainActivity.class);
         startActivity(intent);
+    }
+    public void call (View view)
+    {
+            String number = "108";
+            Uri call = Uri.parse("tel:" + number);
+            Intent surf = new Intent(Intent.ACTION_DIAL, call);
+            startActivity(surf);
     }
 }
--- app/src/main/java/com/example/mims/sos/Login.java
+++ app/src/main/java/com/example/mims/sos/Login.java
@@ -1,7 +1,5 @@
 package com.example.mims.sos;
 
-import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 
@@ -20,9 +18,6 @@ public class Login extends MainActivity
 
     public void mainactivity (View view)
     {
-        String number = "9900236246";
-        Uri call = Uri.parse("tel:" + number);
-        Intent surf = new Intent(Intent.ACTION_DIAL, call);
-        startActivity(surf);
+
     }
 }
